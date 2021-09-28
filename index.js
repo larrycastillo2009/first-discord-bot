f0e1582a58e91b852f5ef52d6806f717acfc9a13
Larry Castillo <larrycastillo2009@gmail.com>
2021-09-28 09:22:36 -0500

    API integrted

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const {Client, Intents} = require('discord.js'); //import discord.js
 
 const client = new Discord.Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]}); //create new client
 
+async function getMeme() {
+    const res = await axios.get('https://memeapi.pythonanywhere.com/');
+    return res.data.memes[0].url;
+}
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
@@ -25,10 +30,6 @@ client.on('message', async msg => {
     }
 });
 
-async function getMeme(){
-    const res = await axios.get('https://memeapi.pythonanywhere.com/');
-    return res.data.memes[0].url;
-}
 
 //make sure this line is the last line
 client.login(process.env.CLIENT_TOKEN); //login bot using token
